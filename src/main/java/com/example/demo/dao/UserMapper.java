7ff0566319d754b07e710c97fd4236a91000ad36
jin <chouanan0126@gmail.com>
2024-04-03 16:39:08 +0900

    2024年4月3日学习

--- src/main/java/com/example/demo/dao/UserMapper.java
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -1,26 +1,19 @@
 package com.example.demo.dao;
 import java.util.List;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.entity.User;
 
 @Mapper
 public interface UserMapper {
-
-    @Select("SELECT * FROM user WHERE id = #{id}")
+   
     public User getUserById(Integer id);
 
-    @Select("SELECT * FROM user")
     public List<User> findAllUsers();
     
-    @Insert("INSERT INTO user (name, age, gender, phone) VALUES (#{name}, #{age}, #{gender}, #{phone})")
     public void insertUser(User user);
 
-    @Update("UPDATE user SET name=#{name}, age=#{age}, gender=#{gender}, phone=#{phone} WHERE id=#{id}")
      public void updateUser(User user);
 
      public Integer deleteUser(Integer id);
