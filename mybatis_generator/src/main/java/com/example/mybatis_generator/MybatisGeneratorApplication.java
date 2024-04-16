0ff5d7e981db1ed55fb43b0a52800c405b2cc399
zhang <chouanan0126@gmail.com>
2024-04-16 13:30:29 +0900

    mybatis-generator单表练习

---
+++ mybatis_generator/src/main/java/com/example/mybatis_generator/MybatisGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.example.mybatis_generator;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisGeneratorApplication.class, args);
+	}
+
+}
